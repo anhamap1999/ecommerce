ce87d0915d1ab484927fab6709f6924d4022d44b
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-11-18 08:21:52 +0700

    user model: update

--- backend/modules/users/user.model.js
+++ backend/modules/users/user.model.js
@@ -1,25 +1,80 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+
+const TokenSchema = new mongoose.Schema({
+  token: {
+    type: String,
+  },
+});
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type :String ,
-        required : true
-    },
-    email :{
-        type :String ,
-        required : true,
-        unique :true,
-        dropDups: true
-    },
-    password:{
-        type :String ,
-        required : true,
-    },
-    isAdmin:{
-        type : Boolean,
-        required : true,
-        default  : false
-    }
+  //   name: {
+  //     type: String,
+  //     required: true,
+  //   },
+  username: {
+    type: String,
+    minlength: 8,
+    maxlength: 30,
+    // required: true,
+    index: true,
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      'Username must only contains alphabet, number, underscore and dot. Underscore and dot can not be inside, at the beginning and the end.',
+    ],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    dropDups: true,
+    maxlength: 50,
+    lowercase: true,
+    match: [/[\w]+?@[\w]+?\.[a-z]{2,4}/, 'Email must have correct format!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 100,
+  },
+  phone_number: {
+    type: String,
+    maxlength: 10,
+    // match: [/^0[0-9]{8}$/, 'Phone number must have correct format!'],
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  status: {
+    type: String,
+    default: 'active',
+    enum: ['active', 'disabled'],
+  },
+  role: {
+    type: String,
+    default: 'customer',
+    enum: ['customer', 'admin'],
+  },
+  full_name: {
+    type: String,
+    maxlength: 50,
+  },
+  gender: {
+    type: String,
+    default: 'female',
+    enum: ['female', 'male', 'other'],
+  },
+  birthday: {
+    type: String,
+  },
+  access_tokens: [TokenSchema],
+  version: {
+    type: Number,
+    default: 0,
+  },
 });
-const userModel = mongoose.model("User" ,userSchema);
-export default userModel;
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
