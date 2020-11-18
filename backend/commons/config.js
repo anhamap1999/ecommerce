16eb3d2de85ef9a10a53436f3e8498692b4cedd5
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-11-18 08:15:11 +0700

    config: add config

---
+++ backend/commons/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const {
+  DB_CONNECT,
+  SECRET_KEY,
+  TOKEN_LIFE,
+  RESET_PASSWORD_SECRET_KEY,
+  RESET_PASSWORD_TOKEN_LIFE,
+  HOST
+} = process.env;
+
+module.exports = {
+  MONGODB_URL: DB_CONNECT,
+  // || 'mongodb://localhost/thanhliem',
+  JWT_SECRET_KEY: SECRET_KEY,
+  // || 'somethingsecret',
+  JWT_TOKEN_LIFE: TOKEN_LIFE,
+  RESET_PASSWORD_SECRET_KEY,
+  RESET_PASSWORD_TOKEN_LIFE,
+  HOST
+};
