8a8a350954aeda3eee771327469de4937b102ca5
A N H <51475160+anhamap1999@users.noreply.github.com>
2021-01-06 08:01:31 +0700

    add upload folder

---
+++ backend/modules/upload/upload.controller.js
@@ -0,0 +1,10 @@
+const db = require('firebase-admin');
+const storage = db.storage();
+
+exports.uploadImage = async (req, res, next) => {
+  try {
+    ////
+  } catch (error) {
+    next(error);
+  }
+};
