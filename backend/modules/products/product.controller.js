0a34a847416b0a502360cd9287feb6dec2b23f54
A N H <51475160+anhamap1999@users.noreply.github.com>
2021-01-13 10:41:27 +0700

    working

--- backend/modules/products/product.controller.js
+++ backend/modules/products/product.controller.js
@@ -8,69 +8,69 @@ const Category = require('../category/category.model');
 
 const _ = require('lodash');
 
-exports.getProduct = async (req, res, next) => {
-  const products = await Product.find({});
-  res.send(products);
-};
-exports.getProductdetails = async (req, res, next) => {
-  const product = await Product.findOne({ _id: req.params.id });
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found.' });
-  }
-};
-exports.saveProduct = async (req, res, next) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    brand: req.body.brand,
-    price: req.body.price,
-    rating: req.body.rating,
-    description: req.body.description,
-    stock: req.body.stock,
-    numReviews: req.body.numReviews,
-  });
+// exports.getProduct = async (req, res, next) => {
+//   const products = await Product.find({});
+//   res.send(products);
+// };
+// exports.getProductdetails = async (req, res, next) => {
+//   const product = await Product.findOne({ _id: req.params.id });
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: 'Product Not Found.' });
+//   }
+// };
+// exports.saveProduct = async (req, res, next) => {
+//   const product = new Product({
+//     name: req.body.name,
+//     image: req.body.image,
+//     brand: req.body.brand,
+//     price: req.body.price,
+//     rating: req.body.rating,
+//     description: req.body.description,
+//     stock: req.body.stock,
+//     numReviews: req.body.numReviews,
+//   });
 
-  const newProduct = await product.save();
-  if (newProduct) {
-    return res
-      .status(201)
-      .send({ message: 'new product created', data: newProduct });
-  } else {
-    return res.status(401).send({ message: 'failed' });
-  }
-};
-exports.updateProduct = async (req, res, next) => {
-  const productId = req.params.id;
-  const product = await Product.findById(productId);
-  if (product) {
-    product.name = req.body.name;
-    product.image = req.body.image;
-    product.brand = req.body.brand;
-    product.price = req.body.price;
-    product.description = req.body.description;
-    product.stock = req.body.stock;
+//   const newProduct = await product.save();
+//   if (newProduct) {
+//     return res
+//       .status(201)
+//       .send({ message: 'new product created', data: newProduct });
+//   } else {
+//     return res.status(401).send({ message: 'failed' });
+//   }
+// };
+// exports.updateProduct = async (req, res, next) => {
+//   const productId = req.params.id;
+//   const product = await Product.findById(productId);
+//   if (product) {
+//     product.name = req.body.name;
+//     product.image = req.body.image;
+//     product.brand = req.body.brand;
+//     product.price = req.body.price;
+//     product.description = req.body.description;
+//     product.stock = req.body.stock;
 
-    const updateproduct = await product.save();
+//     const updateproduct = await product.save();
 
-    if (updateproduct) {
-      return res
-        .status(200)
-        .send({ message: 'Product updated', data: updateproduct });
-    }
-  }
-  return res.status(500).send({ message: 'failed' });
-};
-exports.deteleProduct = async (req, res, next) => {
-  const deletepro = await Product.findById(req.params.id);
-  if (deletepro) {
-    await deletepro.remove();
-    res.send({ msg: 'deleted' });
-  } else {
-    res.send({ msg: 'failed' });
-  }
-};
+//     if (updateproduct) {
+//       return res
+//         .status(200)
+//         .send({ message: 'Product updated', data: updateproduct });
+//     }
+//   }
+//   return res.status(500).send({ message: 'failed' });
+// };
+// exports.deteleProduct = async (req, res, next) => {
+//   const deletepro = await Product.findById(req.params.id);
+//   if (deletepro) {
+//     await deletepro.remove();
+//     res.send({ msg: 'deleted' });
+//   } else {
+//     res.send({ msg: 'failed' });
+//   }
+// };
 
 exports.getProducts = async (req, res, next) => {
   try {
